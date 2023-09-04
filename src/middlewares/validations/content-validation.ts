4c1586ec2ff1d2ee0b3bc2f0a548754ceaf8e435
NeonChrome93 <daarksky1919@gmail.com>
2023-09-04 03:48:51 +0300

    fix post

--- src/middlewares/validations/content-validation.ts
+++ src/middlewares/validations/content-validation.ts
@@ -2,7 +2,7 @@ import {body} from "express-validator";
 import {inputValidationMiddleware} from "./input-validation-middleware"
 
 export const contentValidation = [
-    body('content').notEmpty().withMessage("comment is empty"),
+    //body('content').notEmpty().withMessage("comment is empty"),
     body('content').isString().trim().isLength({min: 20, max: 300}).withMessage('comment not empty and length min 20 max 300'),
 
     inputValidationMiddleware
