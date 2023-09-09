d9ff6ff06730ccdd208218781bdaa2f3cd4cb133
NeonChrome93 <daarksky1919@gmail.com>
2023-09-09 12:49:26 +0300

    fix auth validation

--- src/middlewares/validations/user-registration-email-validation.ts
+++ src/middlewares/validations/user-registration-email-validation.ts
@@ -5,27 +5,34 @@ import {strict} from "assert";
 import {userService} from "../../domain/users-servise";
 
 export const userRegistrationEmailValidation = [
-    body('login').notEmpty().withMessage('incorrect login'),//custom
-    body('login').isString().trim().isLength({min: 3, max: 10}).withMessage('incorrect login'),
-    body('login').custom( async (login: string) => {
+    body('login').isString().trim().isLength({
+        min: 3,
+        max: 10
+    }).withMessage('incorrect login').custom(async (login: string) => {
         const user = await usersRepository.findByLoginOrEmail(login)
-        if(!user) return true
-        if(user.login) throw new Error("user already exist")
+        if (!user) return true
+        if (user.login) throw new Error("user with this login already exist")
 
 
     }),
-    body('password').notEmpty().withMessage('password is required'),
-    body('password').isString().trim().isLength({min: 6, max: 10}).withMessage('incorrect password'),
-    body('email').notEmpty().withMessage('incorrect email'),//custom
-    body('email').isURL().withMessage('incorrect email'),
-    body('email').isString().trim().isLength({max: 150}).withMessage('incorrect email'),
-    body('email').custom(async (email: string) => {
+    body('password').isString().trim().notEmpty().isLength({min: 6, max: 10}).withMessage('incorrect password'),
+
+    body('email').isString().trim().isLength({min: 6, max: 10}).isEmail().custom(async (email: string) => {
         const user = await usersRepository.readUserByEmail(email)
-        if(!user) return true
-        if(user.isConfirmed) throw new Error("email already confirmed")
-
+        if (!user) return true
+        throw new Error('user with this email already created')
 
     }),
+    // body('email').notEmpty().withMessage('incorrect email'),//custom
+    // body('email').isURL().withMessage('incorrect email'),
+    // body('email').isString().trim().isLength({max: 150}).withMessage('incorrect email'),
+    // body('email').custom(async (email: string) => {
+    //     const user = await usersRepository.readUserByEmail(email)
+    //     if(!user) return true
+    //     if(user.isConfirmed) throw new Error("email already confirmed")
+    //
+    //
+    // }),
 
     inputValidationMiddleware
 ]
