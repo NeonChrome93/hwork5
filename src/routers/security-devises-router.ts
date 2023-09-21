538ae27baccc46c4096d5a27632057e297c21d5c
NeonChrome93 <daarksky1919@gmail.com>
2023-09-21 22:16:40 +0300

    add security-devises-router get, delete

---
+++ src/routers/security-devises-router.ts
@@ -0,0 +1,19 @@
+import {Router} from "express";
+import {Request, Response} from "express";
+
+
+export const securityDevisesRouter = new Router({})
+
+
+securityDevisesRouter.get('/devises', (req: Request, res: Response) => {
+
+})
+
+securityDevisesRouter.delete('/devises', (req: Request, res: Response) => {
+
+})
+
+securityDevisesRouter.delete('/devises/:id',(req: Request, res: Response) => {
+
+})
+
