b0feb33ad286b7e52a653fdf51ac8a7d4b1d0bed
NeonChrome93 <daarksky1919@gmail.com>
2023-09-26 22:19:55 +0300

    add deviceId Request

--- src/insex.d.ts
+++ src/insex.d.ts
@@ -1,9 +1,11 @@
 import {UserDbModel} from "./models/users-models/user.models";
+import {DevicesDBType} from "./models/devices-models";
 
 declare global {
     namespace Express {
         export interface Request {
             user:  UserDbModel | null
+            deviceId: string | null
         }
     }
 }
