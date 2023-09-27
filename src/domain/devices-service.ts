2f9668e6b6fdc9defe6fa0fff7a2e42a49957053
NeonChrome93 <daarksky1919@gmail.com>
2023-09-27 23:57:13 +0300

    add device-service logic

--- src/domain/devices-service.ts
+++ src/domain/devices-service.ts
@@ -1,9 +1,13 @@
 import {devicesRepository} from "../repositories/devices/devices-repository";
+import {devicesCollection} from "../db/database";
+import {UserDbModel} from "../models/users-models/user.models";
+import {usersRepository} from "../repositories/users/users-repository-database";
+import {DevicesDBType} from "../models/devices-models";
 
 export const devicesService = {
 
 
-    async createDevice(ip: string, id: string, userid:string, title: string, lastActiveDate: string) {
+    async createDevice(ip: string, id: string, userid: string, title: string, lastActiveDate: string) :Promise<DevicesDBType> {
         const device = {
             ip: ip,
             deviceId: id,
@@ -12,12 +16,30 @@ export const devicesService = {
             lastActiveDate: lastActiveDate
 
         }
-        await devicesRepository.createDevice(device)
+       return  await devicesRepository.createDevice(device)
+
 
     },
-    async deleteDeviceById(deviceId: string){
-       // await devicesRepository.deleteById(deviceId)
-        return;
+
+    async findDeviceById(deviceId: string): Promise<DevicesDBType | null> {
+        return await devicesRepository.findDevice(deviceId)
+    },
+
+    async deleteDeviceExpectCurrent(userId: string, deviceId: string) {
+
+        return await devicesRepository.deleteDeviceExpectCurrent(userId, deviceId)
+    },
+
+
+    async deleteDevicesById(deviceId: string, userId: string): Promise<204 | 403| 404>   {
+        const device = await devicesRepository.findDevice(deviceId)
+        if (!device) return 404
+        if(device.userId !== userId) return 403
+        await devicesRepository.deleteDevicesById(deviceId)
+        return 204
+
+        // return await devicesRepository.deleteDevicesById(deviceId)
+
     }
 
 
