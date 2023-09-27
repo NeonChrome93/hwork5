2c835897215b3ff6a0fac7cb321f1c15a53c23ff
NeonChrome93 <daarksky1919@gmail.com>
2023-09-27 23:56:45 +0300

    add device-router logic

--- src/routers/security-devises-router.ts
+++ src/routers/security-devises-router.ts
@@ -1,19 +1,46 @@
-import {Router} from "express";
-import {Request, Response} from "express";
+import {Request, Response, Router} from "express";
+import {getQueryUserPagination} from "../middlewares/pagination";
+import {userService} from "../domain/users-servise";
+import {devicesRepository} from "../repositories/devices/devices-repository";
+import {jwtService} from "../application/jwt-service";
+import {devicesService} from "../domain/devices-service";
+import {checkRefreshToken} from "../middlewares/auth";
 
 
-export const securityDevisesRouter = new Router({})
+export const securityDevisesRouter = Router({})
 
 
-securityDevisesRouter.get('/devises', (req: Request, res: Response) => {
-
+securityDevisesRouter.get('/devices', checkRefreshToken, async (req: Request, res: Response) => {
+    const refreshToken = req.cookies.refreshToken
+    if (!refreshToken) res.sendStatus(401)
+    const userId = jwtService.getUserIdByToken(refreshToken)
+    const devises = await devicesRepository.findAllUserDevices(userId)
+    res.status(200).send(devises);
+//-> queryRepo-> deviceCollection.find(userId)
+    //сделать миддлвару проверить токен прервать там на 401 статус если нет токена и нет userid checkRefreshToken
 })
 
-securityDevisesRouter.delete('/devises', (req: Request, res: Response) => {
+securityDevisesRouter.delete('/devices', checkRefreshToken, async (req: Request, res: Response) => {
+    console.log(req.deviceId)
+    await devicesService.deleteDeviceExpectCurrent(req.deviceId!.toString(),req.user!._id.toString() )
+    res.sendStatus(204)
 
+//подцепить миддлвару
+//dlete current deviceId
+// -> deviceService.deleteExceptThisOne(userId, currentDeviceId)-> deviceRepo.deleteMany({userId, не currentDeviceId} )
 })
 
-securityDevisesRouter.delete('/devises/:id',(req: Request, res: Response) => {
-
-})
+securityDevisesRouter.delete('/devices/:id', checkRefreshToken,
+    async (req: Request, res: Response) => {
+        const deviceId = req.params.id
+        const status = await devicesService.deleteDevicesById(deviceId, req.user!._id.toString())
+        return res.sendStatus(status)
+        // if (isDeleted) {
+        //     res.sendStatus(204)
+        // } else res.sendStatus(404) //подцепить миддлару с проверкой
+        //
+        // If try to delete the deviceId of other user -найти девайс по девайс если его нет 404, вытащить юзер ид и сравнить юзер
+        //ид у девайся с юзер ид в токене если они не равны кинуть 403
+//-> deviceService-> deleteDeviceById
+    })
 
