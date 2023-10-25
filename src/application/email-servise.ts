290e27f2047710aa242ba5b0ac63dde3c7fab2dc
NeonChrome93 <daarksky1919@gmail.com>
2023-10-25 23:56:58 +0300

    add resendEmail method

--- src/application/email-servise.ts
+++ src/application/email-servise.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import {randomUUID} from "crypto";
 
 export const emailService = {
 
@@ -28,5 +29,32 @@ export const emailService = {
         });
 
         return;
-    }
+    },
+
+    async resendEmail( email: string, passwordRecoveryCode: string, expirationDateOfRecoveryCode: string) {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+                user: 'daarksky1919@gmail.com',
+                pass: 'xkechhtugnbjckly' //gmail key
+            }
+        });
+
+// async..await is not allowed in global scope, must use a wrapper
+
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+            from: 'Yarrow Dark 👻 <daarksky1919@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: "Hello samurai ✔", // Subject line
+            text: "sifhseiofhseif jweipfhwei pfhweifwehif", // plain text body
+            html: `<h1>Thank for your registration</h1>
+            <p>To finish registration please follow the link below:
+            <a href='https://somesite.com/confirm-email?code=${passwordRecoveryCode}'>complete registration</a>
+            </p>`, // html body
+        });
+
+        return;
+    },
 }
