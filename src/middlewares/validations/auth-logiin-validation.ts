084b065b25b3646c68d2e72375c356521858474b
NeonChrome93 <daarksky1919@gmail.com>
2023-10-27 23:15:01 +0300

    fix validation

--- src/middlewares/validations/auth-logiin-validation.ts
+++ src/middlewares/validations/auth-logiin-validation.ts
@@ -2,10 +2,20 @@ import {body} from "express-validator";
 import {inputValidationMiddleware} from "./input-validation-middleware";
 
 export const validationLoginAuth = [
-    body('loginOrEmail').notEmpty().withMessage('incorrect login or email'),
-    body('loginOrEmail').isString().trim().isLength({min: 3, max: 150}).withMessage('incorrect login or email'),
-    body('password').notEmpty().withMessage('password is required'),
-    body('password').isString().trim().isLength({min: 6, max: 10}).withMessage('incorrect password'),
+    body('loginOrEmail')
+        .notEmpty()
+        .withMessage('incorrect login or email')
+        .trim()
+        .isString()
+        .isLength({min: 3, max: 150})
+        .withMessage('incorrect login or email'),
+    body('password')
+        .notEmpty()
+        .withMessage('password is required')
+        .isString()
+        .trim()
+        //.isLength({min: 6, max: 10})
+        .withMessage('incorrect password'),
 
     inputValidationMiddleware
 ]
